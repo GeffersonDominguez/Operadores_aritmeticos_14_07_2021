3aad3d11cad103abe44f14d0d9684d8caf21d6b0
GeffersonDominguez <geffersondominguez93@gmail.com>
2021-07-14 21:29:33 -0500

    Operadores aritmeticos

---
+++ main.js
@@ -0,0 +1,65 @@
+// Ejercicio
+
+// let num1 = parseInt(prompt(`Variable 1`));
+// let num2 = parseInt (prompt(`Variable 2`));
+// console.log (`Variable 1=`, num1);
+// console.log (`Variable 2=`, num2);
+
+// Adición
+
+// num1+=num2
+// console.log(`Adición=`,num1);
+
+// // Predecremento
+// num1-= num2
+// console.log(`Predecremento=`,num1);
+// num1-= num2
+// console.log(`Predecremento=`,num1);
+
+// // Posdecremento
+
+// num1-=num2
+// console.log(`Posdecremento=`,num1);
+// num1-=num2
+// console.log(`Posdecremento=`,num1);
+// num1-=num2
+// console.log(`Posdecremento=`,num1);
+
+// // División
+// num1/=num2
+// console.log(`División=`,num1);
+
+
+
+
+
+
+
+
+
+// let num = 6;
+// console.log(12%8+3*3);
+
+
+// // POS-Incremento
+// ++num;
+// console.log(num);
+// ++num;
+// console.log(num);
+// ++num;
+// console.log(num);
+// ++num;
+
+// console.log(num);
+
+
+// // POS-Incremento
+// num++;
+// console.log(num);
+// num++;
+// console.log(num);
+// num++;
+// console.log(num);
+// num++;
+
+// console.log(num);
